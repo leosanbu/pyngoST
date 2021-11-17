1e29b62c11ff795ab774ed6ae150a32935925dce
Leonor Sánchez Busó <leosanbu@MacBook-Pro-de-Leonor.local>
2021-11-17 15:09:41 +0100

    Upload scripts

--- pyngoST.py
+++ pyngoST.py
@@ -6,26 +6,35 @@ import argparse as arg
 import concurrent.futures
 from pyngoST_functions import * 
 
-parser = arg.ArgumentParser(description='pyngoST: high throughput retrieval of sequence types from N. gonorrhoeae assembly collections', usage = '%(prog)s [options]')
-parser.add_argument('-i', '--input', help='Assembly files', required=False, nargs='+') # add multiple assemblies
-parser.add_argument('-r', '--read_file', help='Read file containing the paths to the input files', required=False) # use if too many input assemblies or several input paths
-parser.add_argument('-s', '--schemes', help='Typing schemes to query: NG-STAR, MLST, cgMLST (default=MLST,NG-STAR)', required=False, default='MLST,NG-STAR')
-parser.add_argument('-c', '--ngstarcc', help='Include NG-STAR clonal complexes (NG-STAR CCs)', required=False)
-parser.add_argument('-p', '--path', help='Path to database containing alleles and profiles', required=True)
+parser = arg.ArgumentParser(description='pyngoST: high throughput molecular typing of N. gonorrhoeae genome assemblies', usage = '%(prog)s [options]')
+parser.add_argument('-i', '--input', help='Assembly files in fasta format', required=False, nargs='+')
+parser.add_argument('-r', '--read_file', help='File containing the paths to the input files', required=False)
+parser.add_argument('-s', '--schemes', help='Typing schemes to query separated by commas (options: NG-STAR, MLST, NG-MAST) (default=NG-STAR)', required=False, default='NG-STAR')
+parser.add_argument('-c', '--ngstarcc', help='File with the NG-STAR clonal complexes (NG-STAR CCs) database to include NG-STAR CCs in the results table', required=False)
+parser.add_argument('-g', '--genogroups', help='Calculate NG-MAST genogroups from NG-MAST types (default=False)', required=False, action='store_true')
+parser.add_argument('-p', '--path', help='Path to database containing MLST/NG-STAR alleles and profiles', required=True)
 parser.add_argument('-a', '--alleles_out', help='Print fasta files with new alleles (optional, default: False)', required=False, action='store_true')
-parser.add_argument('-o', '--out_filename', help='Name of file to print output to (optional, default: screen output)', required=False)
+parser.add_argument('-o', '--out_filename', help='Name of file to print the results table to (optional, default: screen output)', required=False)
 parser.add_argument('-u', '--update', help='Re-create .pkl file after updating the database', required=False, action='store_true')
 arg = parser.parse_args()
 
-# Get arguments #
+#####################
+### Get arguments ###
+#####################
+
 out_path = os.getcwd()
 db_path = arg.path
-schemes = arg.schemes.split(',') if arg.schemes else ['MLST', 'NG-STAR']
+schemes = arg.schemes.split(',') if arg.schemes else ['NG-STAR']
 ccsfile = arg.ngstarcc if arg.ngstarcc else False
+genogroups = True if arg.genogroups else False
 allout = True if arg.alleles_out else False
 outfile = arg.out_filename if arg.out_filename else False
 update = True if arg.update else False
 filelist = getInput(arg.input, arg.read_file)
+
+###############################
+### Update pickled database ###
+###############################
 
 if update:
 	# Only update the pickle file if requested
@@ -37,19 +46,17 @@ else:
 	# Load pickled database - dictionary and automaton    
 	allelesDB, allelesAC = pickle.load(open(db_path+'/'+'ST_alleles_AC.pkl', 'rb'))
 
-## Main script ##
+###################
+### Main script ###
+###################
 
 # order of loci 
 MLSTorder = ['abcZ', 'adk', 'aroE', 'fumC', 'gdh', 'pdhC', 'pgm']
 NGSTARorder = ['penA', 'mtrR', 'porB', 'ponA', 'gyrA', 'parC', '23S']
+NGMASTorder = ['POR', 'TBPB']
 
 # read profiles file
-#if 'MLST' and 'NG-STAR' in schemes:
 profilesDB = readProfiles(db_path)
-#elif schemes==['MLST']:
-#
-#elif schemes==['NG-STAR']:
-
 
 # read NG-STAR CCs
 if ccsfile:
@@ -69,10 +76,34 @@ for s in schemes:
 		tmporder = ['NG-STAR']+NGSTARorder
 		header+=tmporder
 		order+=NGSTARorder
+		if ngstarCCs:
+			header+=['NG-STAR_CC']
+	elif s=='NG-MAST':
+		tmporder = ['NG-MAST']+NGMASTorder
+		header+=tmporder
+		if genogroups:
+			header+=['Genogroup']
 
-if ngstarCCs:
-	header+=['NG-STAR_CC']
+# create tmp/ folder for intermediate operations
+if not 'tmp' in os.listdir(out_path):
+	subprocess.run(['mkdir tmp'], shell=True)
 
+# run NG-MAST and genogroups if requested
+if 'NG-MAST' in schemes:
+	print('## Running NGMASTER...')
+	ngmastDic, ngmastClusters = run_ngmaster(filelist, genogroups)
+	if genogroups:
+		print('## Splitting and aligning POR.fas and TBPB.fas files...')
+		prepare_files_for_genogroups(out_path)
+		print('## Calculating NG-MAST genogroups...')
+		genopergenome = calculateGenogroups(out_path, ngmastClusters)
+	else:
+		genopergenome = None
+else:
+	ngmastDic = None
+	genopergenome = None
+
+# open ouput file and print header
 if outfile:
 	outfilehandle = open(out_path+'/'+outfile, 'w+')
 	outfilehandle.write('\t'.join(header)+'\n')
@@ -80,12 +111,10 @@ else:
 	outfilehandle = None
 	print('\t'.join(header))
 
-# create tmp/ folder for intermediate operations
-subprocess.call(['mkdir', 'tmp'])
-
-# process files
+# process files to call MLST/NG-STAR and add info on NG-MAST/genogroups if requested
 for f in filelist:
-	processFiles(f, order, MLSTorder, NGSTARorder, profilesDB, allelesDB, allelesAC, schemes, ngstarCCs, db_path, allout, out_path, outfile, outfilehandle)
+	processFiles(f, order, MLSTorder, NGSTARorder, profilesDB, allelesDB, allelesAC, schemes, 
+		ngstarCCs, ngmastDic, genopergenome, db_path, allout, out_path, outfile, outfilehandle)
 
 # clean output files
 subprocess.call(['rm', '-r', 'tmp'])
--- pyngoST_functions.py
+++ pyngoST_functions.py
@@ -4,8 +4,9 @@ import subprocess
 import pickle
 import ahocorasick
 import pandas as pd
-from Bio import SeqIO
+from Bio import SeqIO, AlignIO
 from pyfaidx import Fasta
+from Bio.Align.Applications import MuscleCommandline
 
 ## Classes ##
 
@@ -56,9 +57,6 @@ def readProfiles(path):
 			for line in tab:
 				linesplit = line.rstrip().split('\t')
 				st = linesplit[0]
-				#if 'cgMLST' in schemes:
-				#	profile = '\t'.join(linesplit[1:1650])
-				#else:
 				profile = '\t'.join(linesplit[1:8])
 				profilesDB[profile] = st
 		profilesdic[paths[n]] = profilesDB
@@ -167,10 +165,137 @@ def AC_fast(seq, order, allelesDB, allelesAC):
 				results[allelesDB[original_value].gene].append(allelesDB[original_value].allele)
 	return results
 
-def processFiles(fpath, order, MLSTorder, NGSTARorder, profilesDB, allelesDB, allelesAC, schemes, ngstarCCs, db_path, allout, out_path, outfile, outfilehandle):
-	# create a link to the file if in another directory
-	if '/' in fpath:
-		subprocess.call(['ln', '-s', fpath])
+def run_ngmaster(filelist, genogroups):
+	if genogroups:
+		filelist_str = 'ngmaster --printseq ngmaster_printseq.fas '+' '.join(filelist)
+	else:
+		filelist_str = 'ngmaster '+' '.join(filelist)
+	ngmaster_output = subprocess.run(filelist_str, shell=True, capture_output=True).stdout.decode('utf-8')
+	ngmaster_output = ngmaster_output.split('\n')[1:-1]
+	ngmastDic = {}
+	ngmastClusters = {}
+	for i in ngmaster_output:
+		tmp = i.split('\t')
+		ngmastDic[tmp[0]] = '\t'.join(tmp[1:])
+		if genogroups:
+			if not tmp[1] in ngmastClusters:
+				ngmastClusters[tmp[1]] = []
+				#ngmastClusters[tmp[1]] = [tmp[0], tmp[2], tmp[3]]
+			ngmastClusters[tmp[1]].append([tmp[0], tmp[2], tmp[3]])
+	return [ngmastDic, ngmastClusters]
+
+def split_loci_sequences(file, out_path):
+	check_files = os.listdir(out_path)
+	if 'POR.fas' in check_files:
+		subprocess.run(['rm POR.fas'], shell=True)
+	if 'TBPB.fas' in check_files:
+		subprocess.run(['rm TBPB.fas'], shell=True)
+	por_fas = open('POR.fas', 'a')
+	tbpb_fas = open('TBPB.fas', 'a')
+	with open(out_path+'/'+file, 'r') as fasta:
+		for record in SeqIO.parse(fasta, 'fasta'):
+			if 'POR' in record.description:
+				SeqIO.write(record, por_fas, 'fasta')
+			elif 'TBPB' in record.description:
+				SeqIO.write(record, tbpb_fas, 'fasta')
+	por_fas.close()
+	tbpb_fas.close()
+
+def align_sequences(infilename, outfilename): #use maxiters=2 for speed
+	cline = MuscleCommandline("muscle", input=infilename, out=outfilename) #maxiters=maxiters
+	try:
+		stdout, stderr = cline()
+	except OSError:
+		print("Alignment failed:")
+		print(cline)
+		sys.exit()
+
+def calculate_distances(aln):
+	align = AlignIO.read(aln, "fasta")
+	dist={}
+	for recorda in align:
+		seqa=str(recorda.seq).upper()
+		dist[recorda.id]={}
+		for recordb in align:
+			seqb=str(recordb.seq).upper()
+			count=0
+			for x in range(len(seqa)):
+				if seqa[x]!=seqb[x] and seqa[x] in ["A","C","G","T"] and seqb[x] in ["A","C","G","T"]:
+					count+=1
+			dist[recorda.id][recordb.id]=count
+	return dist
+
+def prepare_files_for_genogroups(out_path):
+	split_loci_sequences('ngmaster_printseq.fas', out_path)
+	align_sequences('POR.fas', 'POR.aln')
+	align_sequences('TBPB.fas', 'TBPB.aln')
+
+def calculateGenogroups(out_path, ngmastClusters):
+	por_dist = calculate_distances('POR.aln')
+	tbpb_dist = calculate_distances('TBPB.aln')
+	# calculate clusters
+	clusters=[]
+	for st in ngmastClusters:
+		if st is not '-':
+			s=ngmastClusters[st][0]+[st]
+			hit_clusters=set([])
+			for x, cluster in enumerate(clusters):
+				for c in cluster:
+					hit=False
+					if s[0] in por_dist and s[0] in tbpb_dist:
+						if c[0] in por_dist[s[0]] and c[0] in tbpb_dist[s[0]]:
+							if por_dist[s[0]][c[0]]<=5 and tbpb_dist[s[0]][c[0]]==0:
+								hit=True
+							elif tbpb_dist[s[0]][c[0]]<=4 and por_dist[s[0]][c[0]]==0:
+								hit=True
+							elif tbpb_dist[s[0]][c[0]]+por_dist[s[0]][c[0]]<=8:
+								hit=True
+							if hit:
+								hit_clusters.add(x)
+								continue
+			hit_clusters=list(hit_clusters)
+			if len(hit_clusters)==0:
+				clusters.append([s])
+			elif len(hit_clusters)==1:
+				clusters[hit_clusters[0]].append(s)
+			else:
+				hit_clusters.sort()
+				clusters[hit_clusters[0]].append(s)
+				for c in hit_clusters[1:]:
+					clusters[hit_clusters[0]]=clusters[hit_clusters[0]]+clusters[c]
+				hit_clusters.reverse()
+				for c in hit_clusters[:-1]:
+					clusters.pop(c)
+	# get genogroups
+	genogroups={}
+	output=open("STs_in_genogroups.tsv", "w")
+	for cluster in clusters:
+		g=set([])
+		for c in cluster:
+			g.add(int(c[-1]))
+		sts=[]
+		for c in g:
+			sts.append([len(ngmastClusters[str(c)]), str(c)])
+		sts.sort()
+		sts.reverse()
+		genogroup_name="G"+str(sts[0][1])
+		stssorted = []
+		for s in sts:
+			genogroups[s[1]]=genogroup_name
+			stssorted.append(s[1]+'('+str(s[0])+')')
+		print(genogroup_name, '; '.join(map(str,stssorted)), file=output, sep='\t')
+	output.close()
+	genopergenome = {}
+	for n in ngmastClusters:
+		for sample in ngmastClusters[n]:
+			if n is not '-':
+				genopergenome[sample[0]] = genogroups[n]
+			else:
+				genopergenome[sample[0]] = '-'
+	return genopergenome
+
+def processFiles(fpath, order, MLSTorder, NGSTARorder, profilesDB, allelesDB, allelesAC, schemes, 
+	ngstarCCs, ngmastDic, genopergenome, db_path, allout, out_path, outfile, outfilehandle):
 	fname = fpath.split('/').pop()
 	with open(fpath, 'r') as fasta:
 		concat = ''
@@ -190,8 +315,6 @@ def processFiles(fpath, order, MLSTorder, NGSTARorder, profilesDB, allelesDB, al
 					closest_allele, coords, contigloc = blastout
 					resultsDB[x] = closest_allele
 					save_new_alleles[x] = printNewAlleleSeqs(x, coords, contigloc, fpath, allout, out_path)
-	if '/' in fpath:
-		subprocess.call(['unlink', fname])
 	# get profile and assign ST
 	defaultST = 'NEW'
 	st_list = {}
@@ -209,12 +332,19 @@ def processFiles(fpath, order, MLSTorder, NGSTARorder, profilesDB, allelesDB, al
 				st = '-'
 			else:
 				st = profilesDB['NGSTAR'].get(prof, defaultST)
-				if ngstarCCs:
-					stcc = ngstarCCs[st]
 			if ngstarCCs:
-				st_list['NG-STAR'] = st+'\t'+prof+'\t'+stcc
+				if st=='NEW' or st=='-':
+					st_list['NG-STAR'] = st+'\t'+prof+'\t'+'-'
+				else:
+					st_list['NG-STAR'] = st+'\t'+prof+'\tCC'+ngstarCCs[st]
 			else:		
 				st_list['NG-STAR'] = st+'\t'+prof
+		elif s=='NG-MAST':
+			ngmast = ngmastDic[fpath]
+			if genopergenome:
+				st_list['NG-MAST'] = ngmast+'\t'+genopergenome[fpath]
+			else:
+				st_list['NG-MAST'] = ngmast
 	outline = ''
 	for s in st_list:
 		outline += '\t'+st_list[s]
@@ -222,4 +352,11 @@ def processFiles(fpath, order, MLSTorder, NGSTARorder, profilesDB, allelesDB, al
 		outfilehandle.write(fname+outline+'\n')
 	else:
 		print(fname+outline)
-	
+
+
+
+
+
+
+
+
